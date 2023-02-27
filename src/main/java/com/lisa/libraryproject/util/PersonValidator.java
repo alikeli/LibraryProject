2eb94040a42ea145b6e314a196beb2ae43fca471
alikeli <eli-com2@yandex.ru>
2023-02-27 16:27:23 +0100

    add validator

--- src/main/java/com/lisa/libraryproject/util/PersonValidator.java
+++ src/main/java/com/lisa/libraryproject/util/PersonValidator.java
@@ -1,4 +1,32 @@
 package com.lisa.libraryproject.util;
 
-public class PersonValidator {
+import com.lisa.libraryproject.model.Person;
+import com.lisa.libraryproject.service.PeopleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+@Component
+public class PersonValidator implements Validator {
+
+    private final PeopleService peopleService;
+
+    @Autowired
+    public PersonValidator(PeopleService peopleService) {
+        this.peopleService = peopleService;
+    }
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return Person.class.equals(clazz);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        Person person=(Person) target;
+        if(peopleService.getPersonByFullName(person.getFullName()).isPresent())
+            errors.rejectValue("fullName", "", "Person already is exist");
+
+    }
 }
