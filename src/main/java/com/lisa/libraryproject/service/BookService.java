5dc30c81b0bb9d0e66690d8a20edf486e8badc30
alikeli <eli-com2@yandex.ru>
2023-02-27 16:07:31 +0100

    add services

--- src/main/java/com/lisa/libraryproject/service/BookService.java
+++ src/main/java/com/lisa/libraryproject/service/BookService.java
@@ -1,4 +1,64 @@
 package com.lisa.libraryproject.service;
 
+import com.lisa.libraryproject.model.Book;
+import com.lisa.libraryproject.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional(readOnly = true)
 public class BookService {
+    private final BookRepository bookRepository;
+
+    @Autowired
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public List<Book> findAll(boolean sortByYear) {
+        if (sortByYear)
+            return bookRepository.findAll(Sort.by("year"));
+        else
+            return bookRepository.findAll();
+    }
+
+    public List<Book> findWithPagination(Integer page, Integer booksPerPage, boolean sortByYear) {
+        if (sortByYear)
+            return bookRepository.findAll(PageRequest.of(page, booksPerPage, Sort.by("year"))).getContent();
+        else return bookRepository.findAll(PageRequest.of(page, booksPerPage)).getContent();
+    }
+
+    public Book findOne(int id) {
+        Optional<Book> foundBook = bookRepository.findById(id);
+        return foundBook.orElse(null);
+    }
+
+    public List<Book> findByTitle(String query) {
+        return bookRepository.findByTitleStartingWith(query);
+
+    }
+
+    @Transactional
+    public void save(Book book) {
+        bookRepository.save(book);
+    }
+
+    @Transactional
+    public void update(int id, Book updateBook) {
+        Book bookToUpdate = bookRepository.findById(id).get();
+
+        updateBook.setId(id);
+        updateBook.setOwner(bookToUpdate.getOwner());
+        updateBook.setId(id);
+        updateBook.setOwner(bookToUpdate.getOwner());
+
+        bookRepository.save(updateBook);
+
+    }
 }
